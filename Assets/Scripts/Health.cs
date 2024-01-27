8fa91bb911ecdd0e206872aed0302c664600dcc1
brosenmoller <rosenmollerben@gmail.com>
2024-01-27 05:10:01 +0100

    Art implementatie

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,17 +3,24 @@
 public class Health : MonoBehaviour, IDamagable
 {
     [SerializeField] private float maxHealth;
+    [SerializeField] private Material whiteFlash;
+    [SerializeField] private MeshRenderer meshRenderer;
 
     private float health;
+    private Material normalMaterial;
 
     private void Awake()
     {
         health = maxHealth;
+        normalMaterial = meshRenderer.material;
     }
 
     public void TakeDamage(float damage, Player player)
     {
         health -= damage;
+
+        meshRenderer.material = whiteFlash;
+        new Timer(0.2f, () => { meshRenderer.material = normalMaterial; });
 
         if (health < 0)
         {
