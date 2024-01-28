fa71edac3a34db20f67db58a12910b149c960dcb
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2024-01-28 13:08:41 +0100

    DeathMessage
    Skes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -83,7 +83,6 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         movement = Vector2.zero;
-
         foreach (PlayerControlScheme.PlayerAction playerAction in controlSchemesPerLevel[level].AllActions) 
         {
             if (Input.GetKey(playerAction.key))
@@ -131,8 +130,9 @@ public class PlayerController : MonoBehaviour
 
         if (health < 0)
         {
+            gameObject.SetActive(false);
             level--;
-            if (level < 1) { level = 1; }
+            if (level < 1) { level = 0; }
         }
     }
 
@@ -144,6 +144,11 @@ public class PlayerController : MonoBehaviour
         xp = 0;
         level++;
         UpdateUI();
+    }
+
+    public void ResertHealth() 
+    {
+        health = maxHealth;
     }
 
     public void GiveXp(float xp)
